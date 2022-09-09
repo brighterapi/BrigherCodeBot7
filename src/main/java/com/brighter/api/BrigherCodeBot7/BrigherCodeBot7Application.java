c3bebfd2f97c455cf38a12b41c35c6d54d439af3
MilindMehtabb <milind@backbase.com>
2022-09-09 12:34:52 +0530

    Updating twitter 4j configuration using Configuration builder

--- src/main/java/com/brighter/api/BrigherCodeBot7/BrigherCodeBot7Application.java
+++ src/main/java/com/brighter/api/BrigherCodeBot7/BrigherCodeBot7Application.java
@@ -1,5 +1,6 @@
 package com.brighter.api.BrigherCodeBot7;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -7,9 +8,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 @SpringBootApplication
 public class BrigherCodeBot7Application {
+
+	@Value("${oauth.consumerKey}")
+	private String consumerKey;
+
+	@Value("${oauth.consumerSecret}")
+	private String consumerSecret;
+
+	@Value("${oauth.accessToken}")
+	private String accessToken;
+
+	@Value("${oauth.accessTokenSecret}")
+	private String accessTokenSecret;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrigherCodeBot7Application.class, args);
@@ -17,7 +31,13 @@ public class BrigherCodeBot7Application {
 
 	@Bean
 	public Twitter twitter(){
-		return new TwitterFactory().getInstance();
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+				.setOAuthConsumerKey(consumerKey)
+				.setOAuthConsumerSecret(consumerSecret)
+				.setOAuthAccessToken(accessToken)
+				.setOAuthAccessTokenSecret(accessTokenSecret);
+		return new TwitterFactory(cb.build()).getInstance();
 	}
 
 	@Bean
